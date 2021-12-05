8b8bf41d59953c0263ca87db0232afbb947de062
lost-incode <vicktoriaap@gmail.com>
2021-12-06 01:04:13 +0300

    Fix project according to technical task

--- source/js/script.js
+++ source/js/script.js
@@ -8,3 +8,20 @@ navToggle.addEventListener('click', function () {
   navMain.classList.toggle('main-nav--closed');
   navMain.classList.toggle('main-nav--opened');
 });
+
+var smoothLinks = document.querySelectorAll('a[href^="#"]');
+smoothLinks.forEach(function (smoothLink) {
+  smoothLink.addEventListener('click', function (e) {
+    e.preventDefault();
+    var id = smoothLink.getAttribute('href');
+    var linkSelector = 'a[name="' + id.slice(1, id.length) + '"]';
+
+    document.querySelector(linkSelector).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+
+    navMain.classList.toggle('main-nav--closed');
+    navMain.classList.toggle('main-nav--opened');
+  });
+});
